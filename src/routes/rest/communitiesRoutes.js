f545c1927c3ce3d1432f5f823b9564bca766e05e
tomasndlate <tomasndlate@gmail.com>
2024-05-11 13:42:56 +0100

    Get communities with or without search params

--- src/routes/rest/communitiesRoutes.js
+++ src/routes/rest/communitiesRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../../middleware/authMiddleware');
-const { createCommunity } = require('../../controllers/rest/communitiesController');
+const { createCommunity, getCommunities } = require('../../controllers/rest/communitiesController');
 
 // Create community
 router.post('/', authMiddleware, createCommunity);
+router.get('/', getCommunities)
 
 module.exports = router;
